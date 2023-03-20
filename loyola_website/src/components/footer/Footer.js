3c7568424cd5163c51d1c38dae332136a31cf8f8
shashank <shashank200801@gmail.com>
2023-03-20 20:24:30 +0530

    created new branch

--- loyola_website/src/components/footer/Footer.js
+++ loyola_website/src/components/footer/Footer.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <div className='flex flex-row'>
+      <div className='flex flex-column basis-1/2'>
+        
+      </div>
+
+    </div>
+    </>
   )
 }
 
