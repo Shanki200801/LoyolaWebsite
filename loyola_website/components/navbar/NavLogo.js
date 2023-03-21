eb643c2764fb486b673465177567e055d32a4398
Adelicia <a.seq2804@gmail.com>
2023-03-21 19:39:14 +0530

    prelim navbar added

---
+++ loyola_website/components/navbar/NavLogo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from '../../Assets/logo.png';
+import Image from 'next/image';
+
+const NavLogo = () => {
+  return (
+    <div className="flex flex-row items-center">
+      <Image src={Logo} height={100} width={100}/>
+      <h1 className="text-2xl font-bold px-2">Loyola Degree College</h1>
+    </div>
+  )
+};
+
+export default NavLogo;
