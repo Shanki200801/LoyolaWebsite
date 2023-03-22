5a1e42795a25f1822c9e6cae864fae739ee351d7
Siddharth Vijay Sai <67595252+SiddharthSai4701@users.noreply.github.com>
2023-03-22 20:45:36 +0530

    Updated Navbar2

--- loyola_website/pages/index.js
+++ loyola_website/pages/index.js
@@ -8,13 +8,14 @@ import SupportAssistance from '../components/supportAssistance/SupportAssistance
 import { Inter } from 'next/font/google'
 
 import FeaturedRecentEvents from '../components/featuredRecentEvents/FeaturedRecentEvents';
+import Navbar2 from 'components/navbar/Navbar2';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-     <Navbar/>
+     <Navbar2/>
      <WelcomeImage/>
      <Announcements/>
      <WelcomeText/>
