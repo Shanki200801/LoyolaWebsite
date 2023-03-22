4459acd9986388179ad5d158a89b0c592b14573a
shashank <shashank200801@gmail.com>
2023-03-22 22:26:24 +0530

    changed color

--- loyola_website/components/footer/LogoName.js
+++ loyola_website/components/footer/LogoName.js
@@ -6,9 +6,9 @@ const LogoName = () => {
   return (
     <div className="flex flex-row items-center p-4">
       <Image src={Logo} height={100} width={100}/>
-      <div className= "flex flex-col">
-        <h1 className="text-2xl font-bold px-10">Loyola Degree College</h1>
-        <h2 className="text-xl font-bold px-2">Affiliated to Bangalore University</h2>
+      <div className= "flex flex-col items-center">
+        <h1 className="text-xl font-bold px-2 uppercase">Loyola Degree College</h1>
+        <h2 className="text-m font-light px-2 ">Affiliated to Bangalore University</h2>
       </div>
     </div>
   )
