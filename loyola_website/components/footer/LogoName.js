d66be16628f6315596631a72562bbcf7fa2f9257
shashank <shashank200801@gmail.com>
2023-03-22 11:34:25 +0530

    added muicons and preliminary footer

--- loyola_website/components/footer/LogoName.js
+++ loyola_website/components/footer/LogoName.js
@@ -4,11 +4,14 @@ import Image from 'next/image';
 
 const LogoName = () => {
   return (
-    <div className="flex flex-row items-center">
+    <div className="flex flex-row items-center p-4">
       <Image src={Logo} height={100} width={100}/>
-      <h1 className="text-2xl font-bold px-2">Loyola Degree College</h1>
+      <div className= "flex flex-col">
+        <h1 className="text-2xl font-bold px-10">Loyola Degree College</h1>
+        <h2 className="text-xl font-bold px-2">Affiliated to Bangalore University</h2>
+      </div>
     </div>
   )
 };
 
-export default LogoName;
+export default LogoName;  
