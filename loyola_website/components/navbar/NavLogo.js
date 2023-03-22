542b0f437226f22f2f907b52f9282630d8cb2614
Adelicia <a.seq2804@gmail.com>
2023-03-22 17:22:55 +0530

    New logo created

--- loyola_website/components/navbar/NavLogo.js
+++ loyola_website/components/navbar/NavLogo.js
@@ -5,8 +5,11 @@ import Image from 'next/image';
 const NavLogo = () => {
   return (
     <div className="flex flex-row items-center">
-      <Image src={Logo} height={100} width={100}/>
-      <h1 className="text-2xl font-bold px-2">Loyola Degree College</h1>
+      <Image src={Logo} height={30} width={70}/>
+      <div className="flex flex-col items-center">
+        <h1 className="text-2xl font-bold px-2">Loyola Degree College</h1>
+        <h2 className="text-m font-light px-2">Affiliated to Bangalore University</h2>
+      </div>
     </div>
   )
 };
