cd13c84edb63e108db5c1911d9faa20dcaea6c6d
programmingMohit <programming.mohit@gmail.com>
2023-03-23 13:40:50 +0530

    added icons; placed text overlay; hover not implemented; link not implemented

--- loyola_website/components/courses/Courses.js
+++ loyola_website/components/courses/Courses.js
@@ -2,12 +2,19 @@ import React from 'react'
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
 import Grid from '@mui/material/Grid'; // Grid version 1
 import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
-import redBgImg from '../../Assets/coursesBgImage.webp'
 import Image from 'next/image';
 import styles from './courses.module.css'
 import { Grade } from '@mui/icons-material';
+
+//Images
+import redBgImg from '../../Assets/coursesBgImage.webp'
+import artsAndHumanities from '../../Assets/artsAndHumanities.webp'
+import certificateCourses from '../../Assets/certificateCourses.webp'
+import commerce from '../../Assets/commerce.webp'
+
 
 const Courses = () => {
   return (
@@ -33,10 +40,29 @@ const TextComponent = ()=>{
 
 const IconComponent = ()=>{
   return(
-    <div>
-      <Grid container >
-        <Grid xs={12}><p className='grid place-items-center my-20' style={{height: '15vh', width: '80vw', borderWidth: '0.1rem'}}>IconComponent</p></Grid>
-      </Grid>
+    <div style={{marginTop: '10vh', marginBottom: '10vh'}}>
+       <Stack direction="row" spacing={10}>
+       <div className='grid place-items-center'>
+        <div className= "text-purple-100 font-semibold iconTxt text-4xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
+          Arts & Humanities
+        </div>
+        <Avatar className=" opacity-95"alt="Arts & Humanities" src={artsAndHumanities.src} sx={{width:200, height:200}}/>
+       </div>
+
+       <div className='grid place-items-center'>
+        <div className= "text-white font-semibold iconTxt text-4xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
+          Commerce
+        </div>
+        <Avatar className=" opacity-95" alt="Arts & Humanities" src={commerce.src} sx={{width:200, height:200}}/>
+       </div>
+
+       <div className='grid place-items-center'>
+        <div className= "text-white font-semibold iconTxt text-4xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
+          Certificate Courses
+        </div>
+        <Avatar className=" saturate-200 opacity-90" alt="Arts & Humanities" src={certificateCourses.src}  sx={{width:200, height:200}}/>
+       </div>
+      </Stack>
     </div>
   )
 }
