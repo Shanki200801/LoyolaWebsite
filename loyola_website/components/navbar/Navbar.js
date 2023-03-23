4e0296f3743a1ea8917d211fc6c117b5b7623095
Adelicia <a.seq2804@gmail.com>
2023-03-24 00:03:04 +0530

    Subcomponents created within Navbar.js to improve code readability; tailwind classes added; addl styling otw

--- loyola_website/components/navbar/Navbar.js
+++ loyola_website/components/navbar/Navbar.js
@@ -1,190 +1,275 @@
 import React, { useState } from "react";
 import NavLogo from "./NavLogo";
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import styles from "./navbar.module.css";
 import "./navbar.module.css";
 
+// Main Navbar Component
 const Navbar = () => {
   return (
     <div className="navbar bg-navbar-theme text-white sticky top-0 z-20">
-      <div className="flex-1">
-        <a><NavLogo/></a>
-      </div>  
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-
-        {/* About */}
-        <li tabIndex={0}>
-          <a className={styles.primaryLinks}>
-              About
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="bg-white text-black z-10">
-              <li>
-                <a>About University</a>
-              </li>
-              <li>
-                <a>Vision & Mission</a>
-              </li>
-              <li>
-                <a>Crest & Anthem</a>
-              </li>
-              <li>
-                <a>Core Values - Aims/Objectives</a>
-              </li>
-              <li>
-                <a>Management</a>
-              </li>
-            </ul>
-          </li>
-
-        {/* Student */}
-        <li>
-          <a className={styles.primaryLinks}>
-              Student
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="bg-white text-black z-10">
-              <li>
-                <a>Rules & Regulations</a>
-              </li>
-              <li>
-                <a>Note to parents</a>
-              </li>
-              <li>
-                <a>Scholarships</a>
-              </li>
-              <li>
-                <a>Placements</a>
-              </li>
-            </ul>
-          </li>
-
-          {/* Academics */}
-          <li>
-          <a className={styles.primaryLinks}>
-              Academics
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="bg-white text-black z-10">
-              <li>
-                <a>BA</a>
-              </li>
-              <li>
-                <a>BCom</a>
-              </li>
-              <li>
-                <a>Study Of Languages</a>
-              </li>
-              <li>
-                <a>Cert Courses</a>
-              </li>
-              <li>
-                <a>Examination Results</a>
-              </li>
-            </ul>
-          </li>
-
-          {/* Departments */}
-          <li>
-          <a className={styles.primaryLinks}>
-              Departments
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="bg-white text-black z-10">
-              <li>
-                <a>Department of Commerce</a>
-              </li>
-              <li>
-                <a>Department of English</a>
-              </li>
-              <li>
-                <a>Department of Kannada</a>
-              </li>
-              <li>
-                <a>Department of Hindi</a>
-              </li>
-              <li>
-                <a>Department of Physical Education</a>
-              </li>
-              <li>
-                <a>Department of Psychology</a>
-              </li>
-              <li>
-                <a>Department of Political Science</a>
-              </li>
-              <li>
-                <a>Department of Journalism</a>
-              </li>
-            </ul>
-          </li>
-
-          {/* News */}
-          <li>
-          <a className={styles.primaryLinks}>
-              News & Events
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="bg-white text-black z-10">
-              <li>
-                <a>News</a>
-              </li>
-              <li>
-                <a>Events</a>
-              </li>
-              <li>
-                <a>Gallery</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-          <a className={styles.primaryLinks}>
-              Admissions
-            </a>
-          </li>
-        </ul>
-      </div>
+      <NavLogoLink />
+      <NavLinks />
     </div>
+  );
+};
+
+//Navbar Sub-components//
+
+//Navbar Logo Component
+const NavLogoLink = () => {
+  return (
+    <div className="flex-1">
+      <a>
+        <NavLogo />
+      </a>
+    </div>
+  );
+};
+
+//Navbar Primary Links Component
+const NavLinks = () => {
+  return (
+    <div className="flex-none">
+      <ul className="menu menu-horizontal px-1">
+        <NavAbout />
+        <NavStudent />
+        <NavAcads />
+        <NavDepts />
+        <NavNews />
+        <NavAdmissions />
+      </ul>
+    </div>
+  );
+};
+
+//Navbar About Component
+const NavAbout = () => {
+  return (
+    <li tabIndex={0}>
+      <a className="hover:underline hover:underline-offset-4 decoration-red-500 bg-transparent">
+        About
+        <svg
+          className="fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+        >
+          <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+        </svg>
+      </a>
+      <AboutSubMenu />
+    </li>
+  );
+};
+
+//Navbar About Menu Component
+const AboutSubMenu = () => {
+  return (
+    <ul className="bg-white text-black z-10">
+      <li>
+        <a>About University</a>
+      </li>
+      <li>
+        <a>Vision & Mission</a>
+      </li>
+      <li>
+        <a>Crest & Anthem</a>
+      </li>
+      <li>
+        <a>Core Values</a>
+      </li>
+      <li>
+        <a>Management</a>
+      </li>
+    </ul>
+  );
+};
+
+//Navbar Student Component
+const NavStudent = () => {
+  return (
+    <li>
+      <a className="hover:underline hover:underline-offset-4 decoration-red-500 bg-transparent">
+        Student
+        <svg
+          className="fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+        >
+          <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+        </svg>
+      </a>
+      <StudentSubMenu />
+    </li>
+  );
+};
+
+//Navbar Student Menu Component
+const StudentSubMenu = () => {
+  return (
+    <ul className="bg-white text-black z-10">
+      <li>
+        <a>Rules & Regulations</a>
+      </li>
+      <li>
+        <a>Note to parents</a>
+      </li>
+      <li>
+        <a>Scholarships</a>
+      </li>
+      <li>
+        <a>Placements</a>
+      </li>
+    </ul>
+  );
+};
+
+//Navbar Academics Component
+const NavAcads = () => {
+  return (
+    <li>
+      <a className="hover:underline hover:underline-offset-4 decoration-red-500 bg-transparent">
+        Academics
+        <svg
+          className="fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+        >
+          <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+        </svg>
+      </a>
+      <AcadsSubMenu />
+    </li>
+  );
+};
+
+//Navbar Academics Menu Component
+const AcadsSubMenu = () => {
+  return (
+    <ul className="bg-white text-black z-10">
+      <li>
+        <a>BA</a>
+      </li>
+      <li>
+        <a>BCom</a>
+      </li>
+      <li>
+        <a>Study Of Languages</a>
+      </li>
+      <li>
+        <a>Cert Courses</a>
+      </li>
+      <li>
+        <a>Examination Results</a>
+      </li>
+    </ul>
+  );
+};
+
+//Navbar Departments Component
+const NavDepts = () => {
+  return (
+    <li>
+      <a className="hover:underline hover:underline-offset-4 decoration-red-500 bg-transparent">
+        Departments
+        <svg
+          className="fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+        >
+          <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+        </svg>
+      </a>
+      <DeptSubMenu />
+    </li>
+  );
+};
+
+//Navbar Departments Menu Component
+const DeptSubMenu = () => {
+  return (
+    <ul className="bg-white text-black z-10">
+      <li>
+        <a>Department of Commerce</a>
+      </li>
+      <li>
+        <a>Department of English</a>
+      </li>
+      <li>
+        <a>Department of Kannada</a>
+      </li>
+      <li>
+        <a>Department of Hindi</a>
+      </li>
+      <li>
+        <a>Department of Physical Education</a>
+      </li>
+      <li>
+        <a>Department of Psychology</a>
+      </li>
+      <li>
+        <a>Department of Political Science</a>
+      </li>
+      <li>
+        <a>Department of Journalism</a>
+      </li>
+    </ul>
+  );
+};
+
+//Navbar News Component
+const NavNews = () => {
+  return (
+    <li>
+      <a className="hover:underline hover:underline-offset-4 decoration-red-500 bg-transparent">
+        News & Events
+        <svg
+          className="fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+        >
+          <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+        </svg>
+      </a>
+    </li>
+  );
+};
+
+//Navbar News Menu Component
+const NewsSubMenu = () => {
+  return (
+    <ul className="bg-white text-black z-10">
+      <li>
+        <a>News</a>
+      </li>
+      <li>
+        <a>Events</a>
+      </li>
+      <li>
+        <a>Gallery</a>
+      </li>
+    </ul>
+  );
+};
+
+//Navbar Admissions Component
+const NavAdmissions = () => {
+  return (
+    <li>
+      <a className="hover:underline hover:underline-offset-4 decoration-red-500 bg-transparent">
+        Admissions
+      </a>
+    </li>
   );
 };
 
