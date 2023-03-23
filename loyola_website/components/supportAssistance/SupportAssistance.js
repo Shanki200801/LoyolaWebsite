328e82c46fd699285c14aa2f3e2f818ec813d2bb
shashank <shashank200801@gmail.com>
2023-03-23 11:40:02 +0530

    added icons to support and assistance

--- loyola_website/components/supportAssistance/SupportAssistance.js
+++ loyola_website/components/supportAssistance/SupportAssistance.js
@@ -8,7 +8,7 @@ const SupportAssistance = () => {
   
   return (
     <div className=" bg-[url('http://loyolacollegeblr.com/images/bgg.png')] bg-cover py-4 text-white">
-      <h2 className="text-2xl font-bold mb-4 text-center">Support and Assistance</h2>
+      <h2 className="text-2xl font-bold mb-4 text-center"><span className='text-yellow-600'>Support</span> and Assistance</h2>
       <h3 className="text-m font-thin mb-4 px-40 text-center">
         Loyola degree college is sensitive to the changing needs
         of the student body and the teaching faculty.
