b354982c3884f8ecf59f6486f3d6717c6fc450be
shashank <shashank200801@gmail.com>
2023-03-24 01:08:50 +0530

    Added the marquee and changed footer layout

--- loyola_website/components/announcementsMarquee/Announcements.js
+++ loyola_website/components/announcementsMarquee/Announcements.js
@@ -1,20 +1,17 @@
-import { Container } from 'postcss'
-import React from 'react'
+import { Container } from "postcss";
+import React from "react";
 
 const Announcements = () => {
-  return (
-    <div class="flex ml-10 mr-10 ...">
-  <div class="flex-none w-32 bg-blue-800 ...">
-    Announcement
-  </div>
-  <div class="flex-auto w-64 ...">
-  <marquee>
-      <h1>WELCOME TO ST JOSEPHS COLLEGE</h1>
-    </marquee>
-  </div>
-</div>
-  )
-}
+	return (
+		<div class="flex px-10 bg-white">
+			<div class="flex-none w-32 bg-blue-800 ...">Announcement</div>
+			<div class="flex-auto w-64 ...">
+				<marquee>
+					<h1>WELCOME TO ST JOSEPHS COLLEGE</h1>
+				</marquee>
+			</div>
+		</div>
+	);
+};
 
-
-export default Announcements
+export default Announcements;
