ea27fe1fcf94ffb9fa2aa379b67b8e221c508dbc
programmingMohit <programming.mohit@gmail.com>
2023-03-25 08:33:29 +0530

    added links to icons

--- loyola_website/components/courses/Courses.js
+++ loyola_website/components/courses/Courses.js
@@ -31,9 +31,9 @@ const Courses = () => {
 const TextComponent = ()=>{
   return(
     <div>
-      <Grid container >
-        <Grid xs={12}><p className='grid place-items-center mb-30' style={{height: '15vh', width: '80vw', borderWidth: '0.1rem'}}>TextComponent</p></Grid>
-      </Grid>
+      <h1 className='text-3xl font-bold pb-8  drop-shadow-xl  text-white'>
+        <span>Our curriculum offers a brilliant set of degree programs in </span></h1>
+        <p className=' text-center text-5xl text-white tracking-widest border-b-2 py-8 drop-shadow-xl'>ARTS AND COMMERCE</p>
     </div>
   )
 }
@@ -42,26 +42,32 @@ const IconComponent = ()=>{
   return(
     <div style={{marginTop: '10vh', marginBottom: '10vh'}}>
        <Stack direction="row" spacing={10}>
-       <div className='grid place-items-center'>
-        <div className= "text-purple-100 font-semibold iconTxt text-4xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
-          Arts & Humanities
+       <a href="http://www.loyolacollegeblr.com/ba.html">
+        <div className='grid place-items-center'>
+          <div className= "text-purple-100 font-semibold iconTxt text-4xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
+            Arts & Humanities
+          </div>
+          <Avatar className=" opacity-95"alt="Arts & Humanities" src={artsAndHumanities.src} sx={{width:200, height:200}}/>
         </div>
-        <Avatar className=" opacity-95"alt="Arts & Humanities" src={artsAndHumanities.src} sx={{width:200, height:200}}/>
-       </div>
+       </a>
 
+      <a href="http://www.loyolacollegeblr.com/bcom.html">
        <div className='grid place-items-center'>
         <div className= "text-white font-semibold iconTxt text-4xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
           Commerce
         </div>
         <Avatar className=" opacity-95" alt="Arts & Humanities" src={commerce.src} sx={{width:200, height:200}}/>
        </div>
+      </a>
 
+      <a href="">
        <div className='grid place-items-center'>
         <div className= "text-white font-semibold iconTxt text-4xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
           Certificate Courses
         </div>
         <Avatar className=" saturate-200 opacity-90" alt="Arts & Humanities" src={certificateCourses.src}  sx={{width:200, height:200}}/>
        </div>
+      </a>
       </Stack>
     </div>
   )
@@ -69,13 +75,11 @@ const IconComponent = ()=>{
 
 const DeptBtnComponent = () => {
   return (
-    <Stack spacing={2} direction="row">
       <Button className=" font-mono bg-white text-deptBtnColor rounded-sm hover:bg-white hover: text-deptBtnColorHover"
        variant="contained"
         color="success"
         href="http://www.loyolacollegeblr.com/Teaching-Staff.html"
         style={{fontSize: '1.25rem', fontWeight: '500', borderRadius: '2px', padding: '8px 25px 12px'}}>Departments and Majors</Button>
-    </Stack>
   )  
 }
 
