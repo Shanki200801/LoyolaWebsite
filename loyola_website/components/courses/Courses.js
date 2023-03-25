64daac1a8f30de6ef295ee92421234c58b29a766
programmingMohit <programming.mohit@gmail.com>
2023-03-25 10:48:13 +0530

    finished courses component; added sass

--- loyola_website/components/courses/Courses.js
+++ loyola_website/components/courses/Courses.js
@@ -30,10 +30,10 @@ const Courses = () => {
 
 const TextComponent = ()=>{
   return(
-    <div>
-      <h1 className='text-3xl font-bold pb-8  drop-shadow-xl  text-white'>
-        <span>Our curriculum offers a brilliant set of degree programs in </span></h1>
-        <p className=' text-center text-5xl text-white tracking-widest border-b-2 py-8 drop-shadow-xl'>ARTS AND COMMERCE</p>
+    <div className={`${styles.txtComponent}`}>
+      <h1 className='font-bold pb-8  drop-shadow-xl  text-white'>
+        <span className='text-5xl'>Our curriculum offers a brilliant set of degree programs in </span></h1>
+        <p className=' font-mono text-center text-4xl text-white tracking-widest border-b-2 py-8 drop-shadow-xl'>ARTS AND COMMERCE</p>
     </div>
   )
 }
@@ -43,17 +43,17 @@ const IconComponent = ()=>{
     <div style={{marginTop: '10vh', marginBottom: '10vh'}}>
        <Stack direction="row" spacing={10}>
        <a href="http://www.loyolacollegeblr.com/ba.html">
-        <div className='grid place-items-center'>
-          <div className= "text-purple-100 font-semibold iconTxt text-4xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
+        <div className='grid place-items-center transition duration-300 ease-in hover:ease-in hover:scale-110'>
+          <div className= "text-purple-100 font-semibold iconTxt text-3xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
             Arts & Humanities
           </div>
-          <Avatar className=" opacity-95"alt="Arts & Humanities" src={artsAndHumanities.src} sx={{width:200, height:200}}/>
+          <Avatar className="opacity-95 "alt="Arts & Humanities" src={artsAndHumanities.src} sx={{width:200, height:200}}/>
         </div>
        </a>
 
       <a href="http://www.loyolacollegeblr.com/bcom.html">
-       <div className='grid place-items-center'>
-        <div className= "text-white font-semibold iconTxt text-4xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
+       <div className='grid place-items-center transition duration-300 ease-in hover:ease-in hover:scale-110'>
+        <div className= "text-white font-semibold iconTxt text-3xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
           Commerce
         </div>
         <Avatar className=" opacity-95" alt="Arts & Humanities" src={commerce.src} sx={{width:200, height:200}}/>
@@ -61,8 +61,8 @@ const IconComponent = ()=>{
       </a>
 
       <a href="">
-       <div className='grid place-items-center'>
-        <div className= "text-white font-semibold iconTxt text-4xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
+       <div className='grid place-items-center transition duration-300 ease-in hover:ease-in hover:scale-110'>
+        <div className= "text-white font-semibold iconTxt text-3xl" style={{position: 'absolute', zIndex: 1, maxWidth: 200, textAlign: 'center', textShadow: '2px 0 5px #000'}}>
           Certificate Courses
         </div>
         <Avatar className=" saturate-200 opacity-90" alt="Arts & Humanities" src={certificateCourses.src}  sx={{width:200, height:200}}/>
@@ -75,7 +75,7 @@ const IconComponent = ()=>{
 
 const DeptBtnComponent = () => {
   return (
-      <Button className=" font-mono bg-white text-deptBtnColor rounded-sm hover:bg-white hover: text-deptBtnColorHover"
+      <Button className=" font-mono bg-white text-deptBtnColor rounded-sm transition-colors-background-color transition-colors-color hover:bg-deptBtnColor hover:text-white"
        variant="contained"
         color="success"
         href="http://www.loyolacollegeblr.com/Teaching-Staff.html"
