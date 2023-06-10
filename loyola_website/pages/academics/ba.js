60efe21fdf95c1b14324ca887d46c1f193c502e6
programmingMohit <programming.mohit@gmail.com>
2023-06-10 15:25:24 +0530

    style changes

--- loyola_website/pages/academics/ba.js
+++ loyola_website/pages/academics/ba.js
@@ -4,21 +4,41 @@ const BA = ()=>{
     return (
         <div className="bg-white h-screen" >
             <h1 className="flex justify-center pt-10 text-black text-6xl">Bachelor of Arts (BA)</h1>
-            <div id="eligibility">
-                <h2>Eligibility</h2>
+            <div className='grid grid-cols-2 grid-rows-2'>
+                <Eligibility/>
+                <Offerings/>
+            </div>
+            <div id="buttons" className='flex justify-center'>
+                <button id="ba-faculty" >Faculty</button>
+                <button id="ba-admission-redirect">Apply Now</button>
+            </div>
+               
+        </div>
+    )
+}
+
+const Eligibility = ()=>{
+    return(
+        <div id="eligibility" className='bg-amber-400 text-black text-lg ml-16 mt-14 w-2/5 col-span-2 row-span-1 text-center rounded-2xl h-60'>
+                <h2 className='text-3xl underline underline-offset-8'>Eligibility</h2>
                 <p>A Candidate for the BA Degree Courses shall have passed the Pre
                  – University Examination with any combination of subjects, or equivalent 
                  is 12th standard examination of any Other Board. </p>
             </div>
-            <div id="offerings">
-                <h2>BA Courses:</h2>
-                <p>BA Journalism, Psychology</p>
-                <p>BA Journalism, Political Science</p>
+    );
+}
+
+const Offerings = ()=>{
+
+    return (
+        <div id="offerings" className='bg-amber-500 text-black text-lg mr-16 mt-5 w-2/5 justify-self-end  col-span-2 row-span-1 flex items-center flex-col rounded-2xl h-60'>
+            <h2 className='text-3xl underline underline-offset-8'>BA Courses</h2>
+            <ul>
+                <li>BA Journalism, Psychology</li>
+                <li>BA Journalism, Political Science</li>
+            </ul>  
             </div>
-            <button id="ba-faculty">Faculty</button>
-            <button id="ba-admission-redirect">Apply Now</button>   
-        </div>
-    )
+    );
 }
 
 export default BA;
