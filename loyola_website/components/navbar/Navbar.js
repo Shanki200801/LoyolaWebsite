98980665fe040a20728f0e35dcb36775c5ab5c0b
Adelicia <a.seq2804@gmail.com>
2023-06-11 20:47:50 +0530

    changed departments submenu

--- loyola_website/components/navbar/Navbar.js
+++ loyola_website/components/navbar/Navbar.js
@@ -184,7 +184,7 @@ const NavDepts = () => {
     <li>
       <a className={`hover:underline hover:underline-offset-4 hover: decoration-white bg-transparent uppercase ${styles.primaryLinks}`}>
         Departments
-        <svg
+        {/* <svg
           className="fill-current"
           xmlns="http://www.w3.org/2000/svg"
           width="20"
@@ -192,9 +192,9 @@ const NavDepts = () => {
           viewBox="0 0 24 24"
         >
           <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-        </svg>
+        </svg> */}
       </a>
-      <DeptSubMenu />
+      {/* <DeptSubMenu /> */}
     </li>
   );
 };
