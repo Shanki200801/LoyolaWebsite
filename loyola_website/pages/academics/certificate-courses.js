363aaa51591d2de02b9fa23d736c03ff96c0feb2
programmingMohit <programming.mohit@gmail.com>
2023-06-11 13:06:06 +0530

    created blank cert page

---
+++ loyola_website/pages/academics/certificate-courses.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+const Certificate = ()=>{
+    return(
+        <div className="bg-white h-screen" >
+            <h2 className={`flex justify-center pt-10 text-black text-6xl`}>Certificate Courses</h2>
+            <div>Tally ERP 9</div>
+            <div>Advanced Excel</div>
+            <div>Photography</div>
+            <h2 className={`flex justify-center pt-10 text-black text-6xl`}>Study of Languages</h2>
+            <div>English</div>
+            <div>Kannada</div>
+            <div>Hindi</div>
+            <div>Tamil</div>
+            <div>Telugu</div>              
+        </div>
+    );
+}
+
+export default Certificate;
