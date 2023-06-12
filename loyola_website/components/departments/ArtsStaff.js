effe0749e52563fa836b3d26cbeacd1fa88f5cc0
Shanki200801 <shashank200801@gmail.com>
2023-06-12 14:26:07 +0530

    added staff data

--- loyola_website/components/departments/ArtsStaff.js
+++ loyola_website/components/departments/ArtsStaff.js
@@ -7,44 +7,35 @@ const ArtsStaff = () => {
     {
       id: 1,
       profileimage: '/staff-photos/placeholder.png',
-      name: 'MR VIGNESH',
-      designation: 'Assistant Professor and HOD, Department of Political Science',
-      education: 'M A, B Ed, PGDIT, KSET',
-      description: 'Eight years of teaching experience that includes Assistant Professorship at St Aloysius College, Bengaluru.  Director of Mahatma Gandhi Study Centre.'
+      name: 'Fr. Pratap Chandru SJ',
+      designation: 'Lecturer, Department of Psychology and Counselling',
+      education: 'M Sc Psychology',
+      description: 'Two year of teaching experience.  Campus minister and counselor.'
     },
     {
         id: 2,
         profileimage: '/staff-photos/placeholder.png',
-        name: 'MR VIGNESH',
-        designation: 'Assistant Professor and HOD, Department of Political Science',
-        education: 'M A, B Ed, PGDIT, KSET',
-        description: 'Eight years of teaching experience that includes Assistant Professorship at St Aloysius College, Bengaluru.  Director of Mahatma Gandhi Study Centre.'
+        name: 'MR DALU JOSE',
+        designation: 'Lecturer, Department of Journalism',
+        education: 'MA, PGDCA, Web designing ',
+        description: 'Interest: Journalism, Corporate communication, Photography, Social media, Film making.'
       },
       {
         id: 3,
         profileimage: '/staff-photos/placeholder.png',
-        name: 'MR VIGNESH',
-        designation: 'Assistant Professor and HOD, Department of Political Science',
-        education: 'M A, B Ed, PGDIT, KSET',
-        description: 'Eight years of teaching experience that includes Assistant Professorship at St Aloysius College, Bengaluru.  Director of Mahatma Gandhi Study Centre.'
+        name: 'MR LAZARUS LEPCHA',
+        designation: 'Assistant Professor and HOD, Department of Journalism ',
+        education: 'MCJ, NET',
+        description: 'Seven years of teaching experience.  Professional in the field of Print and Broadcast Media.  Won Best Television News Presentation Award in Kerala Published paper on Journalism and authored a book ‘Lockdown Diary: Locked and unlocked stories’.  Interests include documentaries, short films, sports and music.  Loves traveling.'
       },
       {
         id: 4,
         profileimage: '/staff-photos/placeholder.png',
-        name: 'MR VIGNESH',
-        designation: 'Assistant Professor and HOD, Department of Political Science',
-        education: 'M A, B Ed, PGDIT, KSET',
-        description: 'Eight years of teaching experience that includes Assistant Professorship at St Aloysius College, Bengaluru.  Director of Mahatma Gandhi Study Centre.'
-      },
-      {
-        id: 5,
-        profileimage: '/staff-photos/placeholder.png',
-        name: 'MR VIGNESH',
-        designation: 'Assistant Professor and HOD, Department of Political Science',
-        education: 'M A, B Ed, PGDIT, KSET',
-        description: 'Eight years of teaching experience that includes Assistant Professorship at St Aloysius College, Bengaluru.  Director of Mahatma Gandhi Study Centre.'
-      },
-
+        name: 'Mihir Modak ',
+        designation: 'Lecturer, Department of Psychology',
+        education: 'M Sc  ',
+        description: ''
+      }
     
     
   ]
