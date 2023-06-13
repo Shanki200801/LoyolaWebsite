0e0c9418743c03f9d6b3360ef86796422d6e2bba
Siddharth Vijay Sai <67595252+SiddharthSai4701@users.noreply.github.com>
2023-06-14 02:41:09 +0530

    Basic form done. Conditional elements to be added.

---
+++ loyola_website/pages/admissions/form.js
@@ -0,0 +1,288 @@
+import styles from './Form.module.css'
+
+const Form = () => {
+
+
+  return ( 
+        <div className={styles.body}>
+            <h1 className={styles.h1}>Admission Form 2023-24</h1>
+    <p className={styles.p}>Please fill out this form with the required information</p>
+    <form className={styles.form} method="post" action=''>
+        <label className={styles.label} for="course">Course
+          <select className={styles.select} id="course" name="course" >
+            <option value="">--Select--</option>
+            <option value="1">B.Com</option>
+            <option value="2">B.A Journalism, Psychology</option>
+            <option value="3">B.A Journalism, Political Science</option>
+          </select>
+        </label>
+
+      <fieldset className={styles.fieldset}>
+        <label className={styles.label} for="full-name">1. Name (In block letters, exactly as in SSLC Marks Card). <input className={styles.input} id="first-name" name="first-name" type="text" required /></label><br/>
+      </fieldset>
+
+      <fieldset className={styles.fieldset}>
+        <label className={styles.label} for="dob">2. Date of Birth <input id="dob" name="dob" type="date" required className={styles.input} /></label><br />
+        <label className={styles.label} for="place-of-birth">Place of Birth <input className={styles.input} id="place-of-birth" name="place-of-birth" type="text" required /></label><br/>
+      </fieldset>
+
+    <fieldset className={styles.fieldset}>
+      <label className={styles.label} for="gender">3. Gender
+          <select className={styles.select} id="gender" name="gender">
+            <option value="">--Select--</option>
+            <option value="1">Male</option>
+            <option value="2">Female</option>
+            <option value="3">Third Gender</option>
+          </select>
+        </label>
+    </fieldset> 
+
+      <fieldset className={styles.fieldset}>
+      <label className={styles.label} for="bg">4. Blood Group
+          <select className={styles.select} id="bg" name="bg">
+            <option value="">--Select--</option>
+            <option value="1">A+</option>
+            <option value="2">A-</option>
+            <option value="3">B+</option>
+            <option value="4">B-</option>
+            <option value="5">O+</option>
+            <option value="6">O-</option>
+            <option value="7">AB+</option>
+            <option value="8">AB-</option>
+          </select>
+        </label>
+      </fieldset>
+      
+      <fieldset className={styles.fieldset}>
+        <label className={styles.label} for="nationality">5. Nationality <input className={styles.input} id="nationality" name="nationality" type="text" required /></label><br/>
+      </fieldset>
+
+      <fieldset className={styles.fieldset}>
+      <label className={styles.label} for="religion">6. Religion
+          <select className={styles.select} id="religion" name="religion">
+            <option value="">--Select--</option>
+            <option value="1">Sanathana Dharma/ Hinduism</option>
+            <option value="2">Buddhism</option>
+            <option value="3">Sikhism</option>
+            <option value="4">Jainism</option>
+            <option value="5">Christianity</option>
+            <option value="6">Roman Catholic</option>
+            <option value="7">Islam</option>
+            <option value="8">Zorastrianism</option>
+            <option value="9">Judaism</option>
+            <option value="10">Other</option>
+          </select>
+        </label>
+      </fieldset>
+
+      <fieldset className={styles.fieldset}>
+        <label className={styles.label} for="caste">7. Caste <input className={styles.input} id="caste" name="caste" type="text" required /></label><br/>
+        <label className={styles.label} for="subcaste"> Subcaste <input id="subcaste" name="subcaste" type="text" required className={styles.input} /></label>
+ 
+      <label className={styles.label} for="group"> Group
+          <select className={styles.select} id="group" name="group">
+            <option value="">--Select--</option>
+            <option value="1">SC/ST</option>
+            <option value="2">Category I</option>
+            <option value="3">II A</option>
+            <option value="4">II B</option>
+            <option value="5">III A</option>
+            <option value="6">III B</option>
+            <option value="7">GM</option>
+            <option value="8">Other</option>
+          </select>
+        </label>
+      </fieldset>
+
+      <fieldset className={styles.fieldset}>
+      <label className={styles.label} for="house"> 8. House No. <input className={styles.input} id="house" name="house" type="text" required /></label><br/>
+      <label className={styles.label} for="street"> Street <input className={styles.input} id="street" name="street" type="text" required /></label><br/>
+      <label className={styles.label} for="area"> Area <input className={styles.input} id="area" name="area" type="text" required /></label><br/>
+      <label className={styles.label} for="city"> Town/City <input className={styles.input} id="city" name="city" type="text" required /></label><br/>
+      <label className={styles.label} for="pincode"> Pincode <input className={styles.input} id="pincode" name="pincode" type="text" required /></label><br/>
+      <label className={styles.label} for="state"> State
+          <select className={styles.select} id="state" name="state">
+            <option value="">--Select--</option>
+            <option value="1">Karnataka</option>
+            <option value="2">Tamil Nadu</option>
+            <option value="3">Other</option>
+          </select>
+        </label>
+
+        <label className={styles.label} for="nri">Are you a NRI/ Foreigner
+          <select className={styles.select} id="nri" name="nri">
+            <option value="">--Select--</option>
+            <option value="1">Yes</option>
+            <option value="2">No</option>
+          </select>
+        </label>
+      </fieldset>
+
+      <fieldset className={styles.fieldset}>
+      <label className={styles.label} for="spab">Are you specially abled?
+          <select className={styles.select} id="spab" name="spab">
+            <option value="">--Select--</option>
+            <option value="1">Yes</option>
+            <option value="2">No</option>
+          </select>
+        </label>
+      </fieldset>
+
+      <fieldset className={styles.fieldset}>
+        <label className={styles.label} for="ncla">10. Name of College/Institute Last Attended <input className={styles.input} id="ncla" name="ncla" type="text" required /></label><br/>
+        <label className={styles.label} for="pcla"> Place of College/Institute Last Attended <input id="pcla" name="pcla" type="text" required className={styles.input} /></label>
+      </fieldset>
+
+      <fieldset className={styles.fieldset}>
+      <label className={styles.label} for="bg">11. Name of the Examination Passed
+          <select className={styles.select} id="bg" name="qexam">
+            <option value="">--Select--</option>
+            <option value="1">ISC</option>
+            <option value="2">PUC</option>
+            <option value="3">CBSE</option>
+            <option value="4">Other</option>
+          </select>
+        </label>
+
+      </fieldset>
+
+      <fieldset className={styles.fieldset}>
+      <label className={styles.label} for="ymop">12. Year and Month of Passing <input id="ymop" name="ymop" type="text" required className={styles.input} /></label>
+      </fieldset>
+
+      <fieldset className={styles.fieldset}>
+      <label className={styles.label} for="rnqe">13. Register Number in the Qualifying Examination <input id="rnqe" name="rnqe" type="text" required className={styles.input} /></label>
+
+      <table className="table-auto">
+  <thead>
+    <tr>
+      <th>Subject</th>
+      <th>Maximum Marks</th>
+      <th>Marks Obtained</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+    </tr>
+    <tr>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+    </tr>
+    
+    <tr>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+    </tr>
+
+    <tr>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+    </tr>
+
+    <tr>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+    </tr>
+
+    <tr>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+    </tr>
+
+    <tr>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+    </tr>
+    <tr>
+      <td>Total Marks Obtained</td>
+      <td><input type="text" /></td>
+      <td><input type="text" /></td>
+    </tr>
+  </tbody>
+</table>
+
+    <label className={styles.label} for="op">Overall Percentage <input className={styles.input} id="op" name="op" type="text" required /></label><br/>
+
+</fieldset>
+<fieldset className={styles.fieldset}>
+    <p>First Language is English. Select your preferred Second Language.</p>
+        <label className={styles.label} for="bg">14. Language
+          <select className={styles.select} id="bg" name="bg">
+            <option value="">--Select--</option>
+            <option value="1">Additional English</option>
+            <option value="2">Kannada</option>
+            <option value="3">Hindi</option>
+            <option value="4">Telugu</option>
+            <option value="5">Malayalam</option>
+            <option value="6">Tamil</option>
+          </select>
+        </label><br />
+
+        <label className={styles.label} for="smob">Student's Mobile Number <input className={styles.input} id="smob" name="smob" type="text" required /></label><br/>
+        <label className={styles.label} for="semail">Student's Email ID <input className={styles.input} id="semail" name="semail" type="email" required /></label><br/>
+        <label className={styles.label} for="smob">Aadhar Card Number <input className={styles.input} id="smob" name="aadhaarno" type="text" required /></label><br/>
+</fieldset>
+
+
+        <fieldset className={styles.fieldset}>
+        <label className={styles.label} for="mname">15. Mother's Name <input className={styles.input} id="mname" name="mname" type="text" required /></label><br/>
+        <label className={styles.label} for="fname">Father's Name <input className={styles.input} id="fname" name="fname" type="text" required /></label><br/>
+        </fieldset>
+
+        <fieldset className={styles.fieldset}>
+        <label className={styles.label} for="moccup">16. Mother's Occupation <input className={styles.input} id="moccup" name="moccup" type="text" required /></label><br/>
+        <label className={styles.label} for="foccup">Father's Occupation <input className={styles.input} id="foccup" name="foccup" type="text" required /></label><br/>
+        </fieldset>
+
+        <fieldset className={styles.fieldset}>
+        <label className={styles.label} for="pmn">17. Parent's Mobile Number <input className={styles.input} id="pmn" name="pmn" type="text" required /></label><br/>
+        <label className={styles.label} for="panpan">Parent's Alternate Mobile Number <input className={styles.input} id="pan" name="pan" type="text" required /></label><br/>
+        <label className={styles.label} for="pemail">Parent's Email ID (if any) <input className={styles.input} id="pemail" name="pemail" type="email" required /></label><br/> 
+        </fieldset>
+
+        <fieldset className={styles.fieldset}>
+        <label className={styles.label} for="income">18. Annual Income of the Family in Rupees <input className={styles.input} id="income" name="income" type="text" required /></label><br/>
+        </fieldset>
+
+      <fieldset className={styles.fieldset}>
+        <p>19. Local Address (For Outstation Students)</p>
+      <label className={styles.label} for="osshouse">  House No. <input className={styles.input} id="osshouse" name="osshouse" type="text" required /></label><br/>
+      <label className={styles.label} for="ossstreet"> Street <input className={styles.input} id="ossstreet" name="ossstreet" type="text" required /></label><br/>
+      <label className={styles.label} for="ossarea"> Area <input className={styles.input} id="ossarea" name="ossarea" type="text" required /></label><br/>
+      <label className={styles.label} for="osscity"> Town/City <input className={styles.input} id="osscity" name="osscity" type="text" required /></label><br/>
+      <label className={styles.label} for="osspincode"> Pincode <input className={styles.input} id="osspincode" name="osspincode" type="text" required /></label><br/>
+      <label className={styles.label} for="ossstate"> State
+      <input className={styles.input} id="ossstate" name="ossstate" type="text" required /></label><br/>
+      </fieldset>
+
+      <fieldset className={styles.fieldset}>
+        <label className={styles.label} for="cca"> 20. Extra Curricular/ Co Curricular Activities During PUC
+            <textarea className={styles.textarea} id="cca" name="cca" rows="3" cols="30" ></textarea>
+        </label>
+      </fieldset>
+
+      <fieldset className={styles.fieldsetlast}>
+        <label className={styles.label} for="gn">21. Guardian Name <input className={styles.input} id="gn" name="gn" type="text" required /></label><br/>
+        <label className={styles.label} for="gm">Guardian Mobile No. <input className={styles.input} id="gm" name="gm" type="text" required /></label><br/>
+      </fieldset>
+
+
+
+      <input className={styles.inputsubmit} type="submit" value="Submit" />
+    </form>
+
+
+        </div>
+     );
+}
+ 
+export default Form;
--- loyola_website/pages/admissions/index.js
+++ loyola_website/pages/admissions/index.js
@@ -1,75 +1,77 @@
 import Navbar from 'components/navbar/Navbar';
-import styles from './Admissions.module.css';
 import WelcomeImage from 'components/welcomeImage/WelcomeImage';
 import { Announcement } from '@mui/icons-material';
 import Announcements from 'components/announcementsMarquee/Announcements';
 import Link from 'next/link';
-
+import Form from './form';
 const Admissions = () => {
     
     return ( 
         <>
         <Navbar />
         <div className="bg-slate-300 w-full">
-            <h1 className=" text-4xl font-bold pb-5 text-indigo-900 text-center">
-            <span className=" font-bold text-yellow-600"><u>ADMIS</u></span><u>SIONS</u></h1>
+            <h1 className=" text-4xl font-bold pb-5 text-indigo-900 text-center">ADMISSIONS</h1>
             <br /><br />
             <br /><br />
-            <h2 className=" text-2xl font-bold pb-5 text-indigo-900 ml-24">
-            <span className=" font-bold text-yellow-600"><u>ELIGI</u></span><u>BILITY</u></h2>
+            <h2 className=" text-2xl font-bold pb-5 text-indigo-900 ml-24">ELIGIBILITY</h2>
             <div className="text-black mx-40 font-medium leading-relaxed ">
-                • BCom – It is mandatory that the candidate scores a minimum of 55% in the previous board examination.<br />
-                • B.A – It is mandatory that the candidate scores a minimum of 45% in the previous board examination. <br /><br /><br />
+                • BCom  It is mandatory that the candidate scores a minimum of 55% in the previous board examination.<br />
+                • B.A  It is mandatory that the candidate scores a minimum of 45% in the previous board examination. <br /><br /><br />
             </div><br />
             <div>
-                <h2 className="  text-2xl font-bold pb-5 text-indigo-900">
-                <span className=" font-bold text-yellow-600"><u>GENERAL</u></span><u> INSTRUCTIONS</u></h2>
-                <pre className="text-black mx-40 font-medium leading-relaxed "><br />
-                    1. Application must be submitted online only.<br />
-                    2. The applicant should have a valid e-mail id and mobile number. <br />
-                    3. After submitting the application online, take a printout of the same and submit it to the college office on the same or next day. Applying online does not guarantee your seat in the college.<br />
-                    4. The college does not authorize any individual or organisation to process its admissions on its behalf. Candidates are advised to avoid SMS, email, WhatsApp messages and phone calls from unauthorised persons on or off its campus.<br />
-                    5. The registration and application fee are Rs 300, which is to be paid on the day of the submission of the hardcopy of the application and is non-refundable.<br />
-                    6. All admission is provisional, subject to the approval of Bangalore University.<br />
-                    7. The following documents to be carried along with the hard copy of the application form:<br />
-                    <p class={styles.inner}>
-                        i. The photocopies of the Marks card of Class X (PUC/Class XII Marks card and Transfer certificate to be submitted, if you have received only)<br />
-                        ii. Photocopy of Aadhar card.<br />
-                        iii. Photocopy of Caste and Income certificate.<br />
-                        iv. Photocopy of Migration certificate (applicable to the students of other states only)<br />
-                        v. Four passport size photos
-                    </p>
-                </pre>
+                <h2 className="  text-2xl font-bold pb-5 text-indigo-900">GENERAL INSTRUCTIONS</h2>
+                <li>
+                <ol className="text-black mx-40 font-medium leading-relaxed list-decimal">
+                    <li>Application must be submitted online only.</li>
+                    <li>The applicant should have a valid e-mail id and mobile number. </li>
+                    <li>After submitting the application online, take a printout of the same and submit it to the college office on the same or next day. Applying online does not guarantee your seat in the college.</li>
+                    <li>The college does not authorize any individual or organisation to process its admissions on its behalf. Candidates are advised to avoid SMS, email, WhatsApp messages and phone calls from unauthorised persons on or off its campus.</li>
+                    <li>The registration and application fee are Rs 300, which is to be paid on the day of the submission of the hardcopy of the application and is non-refundable.</li>
+                    <li>All admission is provisional, subject to the approval of Bangalore University.</li>
+                    <li>The following documents to be carried along with the hard copy of the application form:</li>
+                    <ol className="ml-32 list-[lower-roman]">
+                       <li>The photocopies of the Marks card of Class X (PUC/Class XII Marks card and Transfer certificate to be submitted, if you have received only)</li>
+                        <li>Photocopy of Aadhar card.</li>
+                       <li>Photocopy of Caste and Income certificate.                    </li>
+                        <li>Photocopy of Migration certificate (applicable to the students of other states only)</li>
+                       <li>Four passport size photos</li>
+                    </ol>
+                </ol>
+                </li>
             </div>
             <br /><br />
             <br /><br />
 
-            <h2 className="  text-2xl font-bold pb-5 text-indigo-900">
-            <span className=" font-bold text-yellow-600"><u>ADMISSION SCHOLARSHIP</u></span><u> INSTRUCTIONS</u></h2>
-            <table className='text-indigo-900 border-solid border-indigo-900 ml-64'>
-                
-                <tr className='text-yellow-600'>
-                    <th>CRITERIA</th>
-                    <th>SCHOLARSHIP</th>
-                </tr>
-                <tr>
-                    <td>Within 3 days of the announcement of PUC results</td>
-                    <td>      10% of the fees</td>
-                </tr>
+            <h2 className=" text-2xl font-bold pb-5 text-indigo-900">ADMISSION SCHOLARSHIP INSTRUCTIONS</h2>
+            <table className="table-auto">
+  <thead>
+    <tr>
+      <th>Song</th>
+      <th>Artist</th>
+      <th>Year</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+      <td>Malcolm Lockyer</td>
+      <td>1961</td>
+    </tr>
+    <tr>
+      <td>Witchy Woman</td>
+      <td>The Eagles</td>
+      <td>1972</td>
+    </tr>
+    <tr>
+      <td>Shining Star</td>
+      <td>Earth, Wind, and Fire</td>
+      <td>1975</td>
+    </tr>
+  </tbody>
+</table>
 
-                <tr>
-                    <td>Within 3 days of the announcement of PUC results and above 85% marks</td>
-                    <td>10% + 5% = 15% of the fees</td>
-                </tr>
 
-                <tr>
-                    <td>Within 3 days of the announcement of PUC results and above 95% marks</td>
-                    <td>10% + 15% = 25% of the fees</td>
-                </tr>
-            </table>
-
-
-            <Link href="/" className='border-opacity-100 text-indigo-900 bg-yellow-600' >Here to apply!!</Link>
+            <Link href="/admissions/form" className='border-opacity-100 text-indigo-900 bg-yellow-600' >Here to apply!!</Link>
         </div>
         </>
      );
