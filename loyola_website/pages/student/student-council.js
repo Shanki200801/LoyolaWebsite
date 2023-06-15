e01559d055a9eccead58678b60cb4db0dc2d4335
programmingMohit <programming.mohit@gmail.com>
2023-06-16 02:07:42 +0530

    completed structure for mobile

--- loyola_website/pages/student/student-council.js
+++ loyola_website/pages/student/student-council.js
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails'; 
+import AddIcon from '@mui/icons-material/Add'; 
+import Button from '@mui/material/Button';
 
 const StuCouncil = ()=>{
 
@@ -45,19 +50,42 @@ const StuCouncil = ()=>{
             </div>
                 );
             })}
-            <div>
-            <p>Students benefit immensely from being involved in student councils - they learn leadership skills, they have an opportunity to gain experience in public speaking, they learn organizational skills and cope with challenges beyond their everyday college life, they learn how to resolve conflict amicably and diplomatically.</p>
-            
-            <p>Most of all they learn how to make a positive impact on the college and community environment.</p>
+            <section className='mt-16'>
+                <div>
+                <p>Students benefit immensely from being involved in student councils - they learn leadership skills, they have an opportunity to gain experience in public speaking, they learn organizational skills and cope with challenges beyond their everyday college life, they learn how to resolve conflict amicably and diplomatically.</p>
+                
+                <p className='mt-5'>Most of all they learn how to make a positive impact on the college and community environment.</p>
+                </div>
+                <div className='mt-5'>
+                    <Accordion>
+                        <AccordionSummary
+                        expandIcon={<AddIcon/>}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                        >
+                        Benefits of Student Council
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <ul className='list-disc p-2 space-y-3'>
+                                <li> It gives students a practical, first-hand understanding of how democracy functions by giving them a voice and a chance to contribute to the functioning of the college.</li>
+                                <li>A democratically elected student council allows students to practice leadership and decision-making skills as well as working hard for the benefit of their college community.</li>
+                                <li>Students have to think about and communicate to the electorate all the qualities that make them ideal candidates for the particular post they are interested in.</li>
+                                <li>The entire process right from submitting their candidature, preparing their speech, convincing the students to vote for them, getting elected, and performing their duties as responsible representatives of the student body implicitly helps students understand the true meaning of democracy.</li>
+                                <li>They take responsibility for voicing the opinions and ideas of the general student body, as well as informing fellow students about current endeavors within the Student Council.</li>
+                                <li>In addition to planning events that contribute to college spirit, the student council is the voice of the student body. They help share student ideas, interests and concerns of the college community.</li>
+
+                            </ul>
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
+                <div>
+                 
+                </div>
+            </section>
+            <div className='flex justify-center'>
+                <Button variant="contained" color="primary"  className='my-10 text-white font-bold bg-gradient-to-tr from-purple-700 to-purple-500'>Student Council Election Report 2022</Button> 
             </div>
-            <h2>Benefits of Student Council</h2>
-            <div>
-            It gives students a practical, first-hand understanding of how democracy functions by giving them a voice and a chance to contribute to the functioning of the college. A democratically elected student council allows students to practice leadership and decision-making skills as well as working hard for the benefit of their college community. Students have to think about and communicate to the electorate all the qualities that make them ideal candidates for the particular post they are interested in. The entire process right from submitting their candidature, preparing their speech, convincing the students to vote for them, getting elected, and performing their duties as responsible representatives of the student body implicitly helps students understand the true meaning of democracy.
-            </div>
-            <div>
-            They take responsibility for voicing the opinions and ideas of the general student body, as well as informing fellow students about current endeavors within the Student Council. In addition to planning events that contribute to college spirit, the student council is the voice of the student body. They help share student ideas, interests and concerns of the college community.
-            </div>
-            <button>Student Council Election Report 2022</button>
+             
         </div>
     );
 }
