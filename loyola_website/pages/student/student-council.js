92350962097e1aae5d85e0f59bce8593e81f1b2a
programmingMohit <programming.mohit@gmail.com>
2023-06-15 22:25:53 +0530

    added structure to council members section

--- loyola_website/pages/student/student-council.js
+++ loyola_website/pages/student/student-council.js
@@ -1,51 +1,50 @@
 import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 const StuCouncil = ()=>{
+
+    const councilMembers = [[{role: "President",
+    name: "Mr  Hemanth V",
+    class: "3rd B Com B"},{role: "Vice-President",
+    name: "Ms Sharal Acquinas",
+    class: "3rd B Com B"}], [{role: "Secretary",
+    name: "Mr. Nikhil Teja",
+    class: "2nd  B Com C"},{role: "Joint  Secretary",
+    name: "Mr. Vivek Kumar Mourya",
+    class: "1st B Com B"}], [{role: "Cultural Secretary",
+    name: "Mr Shan Michael",
+    class: "2nd B A"},{role: "Sport Secretary",
+    name: "Mr Shreekanth R",
+    class: "3rd B Com A"}]];
+
     return (
-        <div className='bg-white text-black'>
-            <h1 className='flex justify-center text-4xl mt-5'>Student Council</h1>
-            <div>
-                Student Councils the world over is student-led civic organizations designed to help promote College spirit and leadership among students. 
+        <div className='bg-white text-black px-6'>
+            <h1 className='flex justify-center text-4xl pt-5'>Student Council</h1>
+            <div className='text-justify mt-10'>
+                Student Councils the world over is student-led civic organizations designed to help promote College spirit and leadership among students. Students benefit immensely from being involved in student councils - they learn leadership skills, they have an opportunity to gain experience in public speaking, they learn organizational skills and cope with challenges beyond their everyday college life, they learn how to resolve conflict amicably and diplomatically.
             </div>
-            <table  >
-                <tbody>
-                <tr>
-                    <td>CLASS</td>
-                    <td>Students Profile</td>
-                    <td>Designation</td>
-                </tr>
-                <tr>
-                    <td>3rd B Com B</td>
-                    <td>Mr  Hemanth V </td>
-                    <td>President</td>
-                </tr>
-                <tr>
-                    <td>3rd BCom B</td>
-                    <td>Ms Sharal Acquinas</td>
-                    <td>Vuice-President</td>
-                </tr>
-                <tr>
-                    <td>2nd  B Com C</td>
-                    <td>Mr. Nikhil Teja</td>
-                    <td>Secretary</td>
-                </tr>
-                <tr>
-                    <td>1st B Com B</td>
-                    <td>Mr. Vivek Kumar Mourya</td>
-                    <td>Joint  Secretary</td>
-                </tr>
-                <tr>
-                    <td>2nd B A</td>
-                    <td>Mr Shan Michael </td>
-                    <td>Cultural Secretary</td>
-                </tr>
-                <tr>
-                    <td>3rd B Com A</td>
-                    <td>Mr Shreekanth R</td>
-                    <td>Sport Secretary</td>
-                </tr>
-                </tbody>
-            </table>
+            <h2 className='flex justify-center text-xl my-5'>Council Members</h2>
+            {councilMembers.map((item,index)=>{
+                return (
+                    <div className='grid grid-cols-2 gap-8 h-48'>
+            <Card className='self-start h-5/6 text-center p-1 grid justify-center items-center'>
+                <CardContent>
+                <p>{item[0].role}</p>
+                <p>{item[0].name}</p>
+                <p>{item[0].class}</p>
+                </CardContent>
+            </Card>
+            <Card className='self-end h-5/6 text-center p-1 grid justify-center items-center'>
+                <CardContent>
+                <p>{item[1].role}</p>
+                <p>{item[1].name}</p>
+                <p>{item[1].class}</p>
+                </CardContent>
+            </Card>
+            </div>
+                );
+            })}
             <div>
             <p>Students benefit immensely from being involved in student councils - they learn leadership skills, they have an opportunity to gain experience in public speaking, they learn organizational skills and cope with challenges beyond their everyday college life, they learn how to resolve conflict amicably and diplomatically.</p>
             
