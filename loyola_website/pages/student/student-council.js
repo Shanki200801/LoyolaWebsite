a601454d634fa431c09bd1ea6fffa69a56205802
programmingMohit <programming.mohit@gmail.com>
2023-06-15 15:31:57 +0530

    added a structure for mobile

--- loyola_website/pages/student/student-council.js
+++ loyola_website/pages/student/student-council.js
@@ -3,14 +3,9 @@ import * as React from 'react';
 const StuCouncil = ()=>{
     return (
         <div className='bg-white text-black'>
+            <h1 className='flex justify-center text-4xl mt-5'>Student Council</h1>
             <div>
-                Student Councils the world over is student-led civic organizations designed to help promote College spirit and leadership among students. Students benefit immensely from being involved in student councils - they learn leadership skills, they have an opportunity to gain experience in public speaking, they learn organizational skills and cope with challenges beyond their everyday college life, they learn how to resolve conflict amicably and diplomatically. Most of all they learn how to make a positive impact on the college and community environment.
-            </div>
-            <div>
-            It gives students a practical, first-hand understanding of how democracy functions by giving them a voice and a chance to contribute to the functioning of the college. A democratically elected student council allows students to practice leadership and decision-making skills as well as working hard for the benefit of their college community. Students have to think about and communicate to the electorate all the qualities that make them ideal candidates for the particular post they are interested in. The entire process right from submitting their candidature, preparing their speech, convincing the students to vote for them, getting elected, and performing their duties as responsible representatives of the student body implicitly helps students understand the true meaning of democracy.
-            </div>
-            <div>
-            They take responsibility for voicing the opinions and ideas of the general student body, as well as informing fellow students about current endeavors within the Student Council. In addition to planning events that contribute to college spirit, the student council is the voice of the student body. They help share student ideas, interests and concerns of the college community.
+                Student Councils the world over is student-led civic organizations designed to help promote College spirit and leadership among students. 
             </div>
             <table  >
                 <tbody>
@@ -51,6 +46,19 @@ const StuCouncil = ()=>{
                 </tr>
                 </tbody>
             </table>
+            <div>
+            <p>Students benefit immensely from being involved in student councils - they learn leadership skills, they have an opportunity to gain experience in public speaking, they learn organizational skills and cope with challenges beyond their everyday college life, they learn how to resolve conflict amicably and diplomatically.</p>
+            
+            <p>Most of all they learn how to make a positive impact on the college and community environment.</p>
+            </div>
+            <h2>Benefits of Student Council</h2>
+            <div>
+            It gives students a practical, first-hand understanding of how democracy functions by giving them a voice and a chance to contribute to the functioning of the college. A democratically elected student council allows students to practice leadership and decision-making skills as well as working hard for the benefit of their college community. Students have to think about and communicate to the electorate all the qualities that make them ideal candidates for the particular post they are interested in. The entire process right from submitting their candidature, preparing their speech, convincing the students to vote for them, getting elected, and performing their duties as responsible representatives of the student body implicitly helps students understand the true meaning of democracy.
+            </div>
+            <div>
+            They take responsibility for voicing the opinions and ideas of the general student body, as well as informing fellow students about current endeavors within the Student Council. In addition to planning events that contribute to college spirit, the student council is the voice of the student body. They help share student ideas, interests and concerns of the college community.
+            </div>
+            <button>Student Council Election Report 2022</button>
         </div>
     );
 }
