e719d440516458a7e1bfeb021e133de0d207d8bf
programmingMohit <programming.mohit@gmail.com>
2023-06-15 13:43:27 +0530

    created student council and report pages

---
+++ loyola_website/pages/student/student-council-report-2022.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+const SCReport2022 = ()=>{
+    return (
+        <div className='bg-white text-black'>
+            <div>
+            On 09/11/22 the Election of the year 2022-23 for student council was conducted. Students were excited to do the voting and the election committee under the guidance of the chief election commissioner Mr. Calvin George the polling booth for the election was arranged. Every year students used to cast their votes through ballet papers but, this year new technology was used in which students casted their votes in electronic mode. The voting started at 11 am and the first votes of the election was casted by the candidates under the guidance of the presiding officer  Dr. Vincent A along with the supervision of our principal  Fr Dr. Alphonse Fernandes  S.J. In the election polling room representatives were present on behalf of each of the  candidates. The press LOY T.V headed by Mr. Lazarus Lepcha covered the entire election process and interacted the voters. At 3 pm Election was completed. The first two round results were announced in the quadrangle by our principal and vice principal it was a very exciting and thrilled movement among the voters aswell as candidates. The candidates were interviewed by LOY T V after the first-round results. Final round results were announced at 4 pm by Mr. Calvin George, the results were as follows:
+            </div>
+            <ol>
+                <li>President- Hemanth V (3RD B. Com B)</li>
+                <li>Vice-president-Sharal Aquinus (3RD B. Com B)</li>
+                <li>Secreatary- Nikhil Teja (2ND B. Com C)</li>
+                <li>Joint Secreatary- Vivek Kumar Mourya (1ST B. Com A)</li>
+                <li>Cultural Secreatary- Shan Michael (2ND B.A)</li>
+                <li>Sports Secreatary- Shreekanth R (3RD B. Com B) </li>
+            </ol>
+            <div>
+            After the announcement of the results the new council members addressed the gathering and presidential speech was given by our principal Fr Dr. Alphonse Fernandes S.J. At the end of the day the photo session of election committee members along with the new student's council members of the year 2022-23 was held.
+            </div>
+        </div>
+    );
+}
+
+export default SCReport2022;
---
+++ loyola_website/pages/student/student-council.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+
+const StuCouncil = ()=>{
+    return (
+        <div className='bg-white text-black'>
+            <div>
+                Student Councils the world over is student-led civic organizations designed to help promote College spirit and leadership among students. Students benefit immensely from being involved in student councils - they learn leadership skills, they have an opportunity to gain experience in public speaking, they learn organizational skills and cope with challenges beyond their everyday college life, they learn how to resolve conflict amicably and diplomatically. Most of all they learn how to make a positive impact on the college and community environment.
+            </div>
+            <div>
+            It gives students a practical, first-hand understanding of how democracy functions by giving them a voice and a chance to contribute to the functioning of the college. A democratically elected student council allows students to practice leadership and decision-making skills as well as working hard for the benefit of their college community. Students have to think about and communicate to the electorate all the qualities that make them ideal candidates for the particular post they are interested in. The entire process right from submitting their candidature, preparing their speech, convincing the students to vote for them, getting elected, and performing their duties as responsible representatives of the student body implicitly helps students understand the true meaning of democracy.
+            </div>
+            <div>
+            They take responsibility for voicing the opinions and ideas of the general student body, as well as informing fellow students about current endeavors within the Student Council. In addition to planning events that contribute to college spirit, the student council is the voice of the student body. They help share student ideas, interests and concerns of the college community.
+            </div>
+            <table  >
+                <tr>
+                    <td>CLASS</td>
+                    <td>Students Profile</td>
+                    <td>Designation</td>
+                </tr>
+                <tr>
+                    <td>3rd B Com B</td>
+                    <td>Mr  Hemanth V </td>
+                    <td>President</td>
+                </tr>
+                <tr>
+                    <td>3rd BCom B</td>
+                    <td>Ms Sharal Acquinas</td>
+                    <td>Vuice-President</td>
+                </tr>
+                <tr>
+                    <td>2nd  B Com C</td>
+                    <td>Mr. Nikhil Teja</td>
+                    <td>Secretary</td>
+                </tr>
+                <tr>
+                    <td>1st B Com B</td>
+                    <td>Mr. Vivek Kumar Mourya</td>
+                    <td>Joint  Secretary</td>
+                </tr>
+                <tr>
+                    <td>2nd B A</td>
+                    <td>Mr Shan Michael </td>
+                    <td>Cultural Secretary</td>
+                </tr>
+                <tr>
+                    <td>3rd B Com A</td>
+                    <td>Mr Shreekanth R</td>
+                    <td>Sport Secretary</td>
+                </tr>
+            </table>
+        </div>
+    );
+}
+
+export default StuCouncil;
