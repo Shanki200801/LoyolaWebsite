48b2ebd3b86db826e603124ffdd98c57fda04727
programmingMohit <programming.mohit@gmail.com>
2023-06-16 23:46:38 +0530

    admissions pages done; uniform fonts implemented

--- loyola_website/pages/admissions/index.js
+++ loyola_website/pages/admissions/index.js
@@ -3,8 +3,7 @@ import admissionsImg from '@/public/admissionsImg/admissions.webp'
 import scholarshipImg from '@/public/admissionsImg/scholarshipImg.webp'
 import Image from 'next/image';
 import Button from '@mui/material/Button';
-import { Allerta_Stencil } from 'next/font/google';
-import { Inter, Oswald} from 'next/font/google';
+import { Allerta_Stencil, Inter, Oswald} from 'next/font/google';
 
 const headingText = Allerta_Stencil({subsets: ['latin'], weight: '400'})
 const bodyText = Inter({subsets: ['latin']});
@@ -13,18 +12,18 @@ const subheading = Oswald({subsets: ['latin']});
 const Admissions = () => {
     
     return ( 
-        <div className='bg-white text-black'>
+        <div className=' text-black to-blue-400 bg-white'>
           <h1 className={`${headingText.className} flex justify-center text-4xl lg:text-6xl pt-8`}>Admissions</h1>
           <EligibilitySub/>
-          <div className={`hidden lg:grid lg:grid-cols-2 lg:px-32 lg:mt-20 lg:gap-10`}>
+          <div className={`hidden lg:grid lg:grid-cols-2 lg:px-32 lg:mt-5 lg:gap-10 lg:py-10 lg:mx-32 bg-gradient-to-br from-amber-400 to-amber-500`}>
             <Image src={admissionsImg} className='lg:w-auto lg:h-full lg:object-cover'/>
             <GenInstructionsSub/>
           </div>
           <div className={`lg:hidden`}>
-            <Image src={admissionsImg} className='mt-10'/>
-            <GenInstructionsSub className="mt-10 px-8"/>
+            <Image src={admissionsImg} />
+            <GenInstructionsSub className="py-10 px-8 bg-gradient-to-br from-amber-400 to-amber-500"/>
           </div>
-          <Image src={scholarshipImg} className='mt-10 lg:hidden' />
+          <Image src={scholarshipImg} className='lg:hidden' />
               <ScholarshipInstructionsSub/>
               <div className={`flex justify-center p-10`}>
                 <Button variant="contained" href="/admissions/form" size="large" className={`text-xl lg:text-3xl lg:p-5 ${subheading.className}`}>
@@ -38,7 +37,7 @@ const Admissions = () => {
 
 const EligibilitySub = ()=>{
   return (
-    <section className={`mt-10 px-6 lg:mt-16 lg:px-32`}>
+    <section className={`mt-10 px-6 lg:mt-16 lg:mx-32 lg:px-5 py-10 bg-gradient-to-tl from-emerald-800 to-emerald-600 text-white`}>
       <h2 className={`${subheading.className} text-2xl lg:text-4xl underline underline-offset-8`}>ELIGIBILITY</h2>
               <div className={` ${bodyText.className} mt-5 lg:mt-10 lg:text-xl`}>
                 <ul className={`space-y-3 lg:text-xl lg:space-y-5`}>
@@ -52,7 +51,7 @@ const EligibilitySub = ()=>{
 
 const GenInstructionsSub = (props)=>{
   return(
-    <section className={`${props.className}`}>
+    <section className={`${props.className} `}>
       <h2 className={` ${subheading.className} text-2xl lg:text-4xl underline underline-offset-8`}>GENERAL INSTRUCTIONS</h2>
                 <ol className={`list-decimal  mt-5`}>
                     <li>Application must be submitted online only.</li>
@@ -77,29 +76,29 @@ const GenInstructionsSub = (props)=>{
 
 const ScholarshipInstructionsSub = ()=>{
   return (
-    <section className={`mt-10 p-6 lg:mt-20 lg:px-32 lg:grid lg:justify-center`}>
+    <section className={` p-6 lg:mt-5 lg:px-32 lg:mx-32 lg:grid lg:justify-center bg-gradient-to-tl from-emerald-800 to-emerald-600 text-white`}>
       <h2 className={`${subheading.className} text-2xl text-center lg:text-4xl underline underline-offset-8 lg:flex lg:justify-center`}>ADMISSION SCHOLARSHIP INSTRUCTIONS</h2>
               <table className={`mt-10`}>
                 <thead>
                   <tr >
-                    <th className='border-2 border-black p-3'>Criteria</th>
-                    <th className='border-2 border-black p-3'>Scholarship</th>
+                    <th className='border-2 border-white p-3'>Criteria</th>
+                    <th className='border-2 border-white p-3'>Scholarship</th>
                   </tr>
                 </thead>
                 <tbody >
                   <tr>
-                    <td className='border-2 border-black p-3'>Within 3 days of the announcement of PUC results</td>
-                    <td className='border-2 border-black p-3'>10% of the fees</td>
+                    <td className='border-2 border-white p-3'>Within 3 days of the announcement of PUC results</td>
+                    <td className='border-2 border-white p-3'>10% of the fees</td>
                     
                   </tr>
                   <tr>
-                    <td className='border-2 border-black p-3'>Within 3 days of the announcement of PUC results and above 85% marks</td>
-                    <td className='border-2 border-black p-3'>10% + 5% = 15% of the fees</td>
+                    <td className='border-2 border-white p-3'>Within 3 days of the announcement of PUC results and above 85% marks</td>
+                    <td className='border-2 border-white p-3'>10% + 5% = 15% of the fees</td>
                     
                   </tr>
                   <tr>
-                    <td className='border-2 border-black p-3'>Within 3 days of the announcement of PUC results and above 95% marks</td>
-                    <td className='border-2 border-black p-3'>10% + 15% = 25% of the fees</td>
+                    <td className='border-2 border-white p-3'>Within 3 days of the announcement of PUC results and above 95% marks</td>
+                    <td className='border-2 border-white p-3'>10% + 15% = 25% of the fees</td>
                   </tr>
                 </tbody>
               </table>
