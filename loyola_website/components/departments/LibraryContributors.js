698c49afb27971da57f6a6252543a977dc5cc801
Siddharth Vijay Sai <67595252+SiddharthSai4701@users.noreply.github.com>
2023-06-17 12:05:47 +0530

    Added library content

---
+++ loyola_website/components/departments/LibraryContributors.js
@@ -0,0 +1,204 @@
+const LibraryContributors = () => {
+
+    const contributors = [
+
+        {
+            id: 1,
+            name: "Mr Santhosh Kumar",
+            amount: 3000,
+            towards: "NSS Fence"
+
+        },
+
+        {
+            id: 2,
+            name: "Fr Hilario",
+            amount: 38250,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 3,
+            name: "Jasmine Angel",
+            amount: 5000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 4,
+            name: "Suthar",
+            amount: 10000,
+            towards: "NSS Fence"
+
+        },
+
+        {
+            id: 5,
+            name: "Eastern Associates",
+            amount: 5000,
+            towards: "NSS Fence"
+
+        },
+
+        {
+            id: 6,
+            name: "Suchitha Anchan",
+            amount: 25000,
+            towards: "NSS Fence"
+
+        },
+
+        {
+            id: 7,
+            name: "Terence Marian D'Souza",
+            amount: 5000,
+            towards: "NSS Fence"
+
+        },
+
+        {
+            id: 8,
+            name: "Maryan",
+            amount: 4000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 9,
+            name: "Renuka",
+            amount: 3000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 10,
+            name: "Alexis",
+            amount: 10000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 11,
+            name: "Naveen",
+            amount: 9000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 12,
+            name: "Philip Andrew Mascarenhas",
+            amount: 3000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 13,
+            name: "Rahul Advertisers" ,
+            amount: 4000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 14,
+            name: "Mathew Moras",
+            amount: 4000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 15,
+            name: "Harish Shenoy",
+            amount: 3000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 16,
+            name: "Allan",
+            amount: 5000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 17,
+            name: "Koshy Nagaland",
+            amount: 5000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 18,
+            name: "C Indu",
+            amount: 5000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 19,
+            name: "Sebastian",
+            amount: 10000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 20,
+            name: "Xavier Board of Higher Education",
+            amount: 10000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 21,
+            name: "D’Souza",
+            amount: 10000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 22,
+            name: "Zelia",
+            amount: 5000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 23,
+            name: "Richard",
+            amount: 10000,
+            towards: "Scholarship"
+
+        },
+
+        {
+            id: 24,
+            name: "IIS",
+            amount: 6500,
+            towards: "Sublaltern"
+
+        }
+
+    ]
+
+    return ( 
+        <></>
+     );
+}
+ 
+export default LibraryContributors;
---
+++ loyola_website/components/departments/LibraryResources.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const LibraryResources = () => {
+
+    const resources = [
+        {
+            total_no_books: 5420,
+            tot_ref_books: 589,
+            SCST_book_bank: 574,
+            competitive_books: 24,
+            donated_books: 1617,
+            tot_national_journals: 5,
+            tot_international_journals: 3,
+            magazines: 8,
+            newspapers: 8
+        }
+
+        
+    ]
+    return ( 
+        <>
+        </>
+
+     );
+}
+ 
+export default LibraryResources;
