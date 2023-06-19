798f812074e8c59ba12a7a4023f70b8fb6897568
Shanki200801 <shashank200801@gmail.com>
2023-06-19 22:26:58 +0530

    removed unwanted imports

--- loyola_website/pages/index.js
+++ loyola_website/pages/index.js
@@ -1,26 +1,19 @@
-
 import Announcement_marquee from "@/components/home/Announcement_marquee";
 import Courses from "../components/home/Courses";
 import WelcomeImage from "../components/home/WelcomeImage";
 import WelcomeText from "../components/home/WelcomeText";
 import SupportAssistance from "../components/home/SupportAssistance";
-import Gallery from '../components/home/Gallery';
-
-
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ['latin'] })
+import Gallery from "../components/home/Gallery";
 
 export default function Home() {
   return (
     <>
-
-			<WelcomeImage />
-			<Announcement_marquee />
-			<WelcomeText />
-			<Gallery />
-			<Courses />
-			<SupportAssistance />
-
+      <WelcomeImage />
+      <Announcement_marquee />
+      <WelcomeText />
+      <Gallery />
+      <Courses />
+      <SupportAssistance />
     </>
-	);
+  );
 }
