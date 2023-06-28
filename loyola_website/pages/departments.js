8dfb07d9ef1a0c464993bca3a659b2a245bad993
Adelicia <a.seq2804@gmail.com>
2023-06-28 23:46:13 +0530

    added backgrounds to departments, news & events, staff achievements

--- loyola_website/pages/departments.js
+++ loyola_website/pages/departments.js
@@ -14,89 +14,93 @@ const departments = ({ staff_details }) => {
   const [departments, setDepartments] = useState("management");
   return (
     <>
-      <div className="flex flex-col pb-8 justify-center bg-gradient-to-tr from-slate-300 to-orange-50">
-        <h1
-          className={`text-6xl font-extrabold my-4 text-center text-shadow-custom-blue text-slate-800 ${lobster.className}  `}
-        >
-          Facilitators
-        </h1>
-        <div className="flex flex-row mx-6">
-          <section className="w-1/5">
-            <div className="w-full text-base font-bold text mt-20  text-right text-navbar-theme ">
-              <button
-                onClick={() => setDepartments("management")}
-                className={` py-1 hover:underline rounded-l-lg  px-4  my-2 ${
-                  departments == "management"
-                    ? "bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white bg-"
-                    : "text-navbar-theme"
-                }`}
-              >
-                Management
-              </button>
-              <br />
-              <button
-                onClick={() => setDepartments("languages")}
-                className={` py-1 hover:underline rounded-l-lg px-4 my-2 ${
-                  departments == "languages"
-                    ? "bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white"
-                    : "text-navbar-theme"
-                }`}
-              >
-                Department of Languages
-              </button>
-              <button
-                onClick={() => setDepartments("commerce")}
-                className={`py-1 hover:underline rounded-l-lg px-4 my-2 ${
-                  departments == "commerce"
-                    ? "bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white"
-                    : "text-navbar-theme"
-                }`}
-              >
-                Department of Commerce
-              </button>
-              <br />
-              <button
-                onClick={() => setDepartments("arts")}
-                className={` py-1 hover:underline rounded-l-lg px-4 my-2 ${
-                  departments == "arts"
-                    ? "bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white"
-                    : "text-navbar-theme"
-                }`}
-              >
-                Department of Arts
-              </button>
-              <br />
-              <button
-                onClick={() => setDepartments("nonteaching")}
-                className={` py-1 hover:underline rounded-l-lg px-4 my-2 ${
-                  departments == "nonteaching"
-                    ? "bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white"
-                    : "text-navbar-theme"
-                }`}
-              >
-                Non-Teaching Staff
-              </button>
-              <br />
-            </div>
-          </section>
+      <div
+        className={`flex flex-col justify-center bg-[url('~/public/backgrounds/bgTwo.webp')] bg-cover bg-center bg-fixed`}
+      >
+        <div className={`bg-blue-100/90 pb-8`}>
+          <h1
+            className={`text-6xl font-extrabold my-4 text-center text-shadow-custom-blue text-slate-800 ${lobster.className}  `}
+          >
+            Facilitators
+          </h1>
+          <div className="flex flex-row mx-6">
+            <section className="w-1/5">
+              <div className="w-full text-base font-bold text mt-20  text-right text-navbar-theme ">
+                <button
+                  onClick={() => setDepartments("management")}
+                  className={` py-1 hover:underline rounded-l-lg  px-4  my-2 ${
+                    departments == "management"
+                      ? "bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white bg-"
+                      : "text-navbar-theme"
+                  }`}
+                >
+                  Management
+                </button>
+                <br />
+                <button
+                  onClick={() => setDepartments("languages")}
+                  className={` py-1 hover:underline rounded-l-lg px-4 my-2 ${
+                    departments == "languages"
+                      ? "bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white"
+                      : "text-navbar-theme"
+                  }`}
+                >
+                  Department of Languages
+                </button>
+                <button
+                  onClick={() => setDepartments("commerce")}
+                  className={`py-1 hover:underline rounded-l-lg px-4 my-2 ${
+                    departments == "commerce"
+                      ? "bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white"
+                      : "text-navbar-theme"
+                  }`}
+                >
+                  Department of Commerce
+                </button>
+                <br />
+                <button
+                  onClick={() => setDepartments("arts")}
+                  className={` py-1 hover:underline rounded-l-lg px-4 my-2 ${
+                    departments == "arts"
+                      ? "bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white"
+                      : "text-navbar-theme"
+                  }`}
+                >
+                  Department of Arts
+                </button>
+                <br />
+                <button
+                  onClick={() => setDepartments("nonteaching")}
+                  className={` py-1 hover:underline rounded-l-lg px-4 my-2 ${
+                    departments == "nonteaching"
+                      ? "bg-gradient-to-r from-blue-600 via-blue-500 to-blue-300 text-white"
+                      : "text-navbar-theme"
+                  }`}
+                >
+                  Non-Teaching Staff
+                </button>
+                <br />
+              </div>
+            </section>
 
-          <section className="mx-auto bg-gradient-to-r from-blue-300 via-blue-100 to-slate-200 rounded-2xl">
-            {departments == "management" && (
-              <ManagementStaff data={staff_details["management-staff"]} />
-            )}
-            {departments == "languages" && (
-              <LanguagesStaff data={staff_details["language-staff"]} />
-            )}
-            {departments == "commerce" && (
-              <CommerceStaff data={staff_details["commerce-staff"]} />
-            )}
-            {departments == "arts" && (
-              <ArtsStaff data={staff_details["arts-staff"]} />
-            )}
-            {departments == "nonteaching" && (
-              <NonTeaching data={staff_details["nonteaching-staff"]} />
-            )}
-          </section>
+            <section className="mx-auto bg-gradient-to-r from-blue-500 via-blue-200 to-blue-500 rounded-2xl">
+              {departments == "management" && (
+                <ManagementStaff data={staff_details["management-staff"]} />
+              )}
+              {departments == "languages" && (
+                <LanguagesStaff data={staff_details["language-staff"]} />
+              )}
+              {departments == "commerce" && (
+                <CommerceStaff data={staff_details["commerce-staff"]} />
+              )}
+              {departments == "arts" && (
+                <ArtsStaff data={staff_details["arts-staff"]} />
+              )}
+              {departments == "nonteaching" && (
+                <NonTeaching data={staff_details["nonteaching-staff"]} />
+              )}
+            </section>
+          </div>
         </div>
       </div>
     </>
--- loyola_website/pages/staff-achievements.js
+++ loyola_website/pages/staff-achievements.js
@@ -6,22 +6,26 @@ const oswald = Oswald({ subsets: ["latin"] });
 const inter = Inter({ subsets: ["latin"] });
 const staffAchievements = ({ achievements }) => {
   return (
-    <div className="bg-gradient-to-tr from-slate-300 to-amber-50 pb-6 ">
-      <h1
-        className={`${lobster.className} custom-letter-spacing-wider text-6xl py-6 text-center text-shadow-custom-blue text-black `}
-      >
-        Staff Achievements
-      </h1>
-      <ul>
-        {achievements.map((achievement) => (
-          <li
-            key={achievement.id}
-            className={`max-w-xl py-8 text-black mx-auto ${inter.className} `}
-          >
-            {achievement.description}
-          </li>
-        ))}
-      </ul>
+    <div
+      className={`bg-[url('~/public/backgrounds/bgTwo.webp')] bg-cover bg-center bg-fixed`}
+    >
+      <div className={`bg-blue-100/90 pb-6`}>
+        <h1
+          className={`${lobster.className} custom-letter-spacing-wider text-6xl py-6 text-center text-shadow-custom-blue text-black `}
+        >
+          Staff Achievements
+        </h1>
+        <ul>
+          {achievements.map((achievement) => (
+            <li
+              key={achievement.id}
+              className={`max-w-xl py-8 text-black mx-auto ${inter.className} `}
+            >
+              {achievement.description}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
